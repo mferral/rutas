e120525b18905bf340c1229df022903d1ed6b825
Miguel Ferral <mferral@gmail.com>
2018-07-26 16:46:26 -0500

    todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { DefaultComponent } from './default/default.component';
+import { Page3Component } from './page3/page3.component';
+import { VoidComponent } from './void/void.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DefaultComponent},
+  {path: 'pagina1', component: Page1Component},
+  {path: 'pagina2', component: Page2Component},
+  {path: 'pagina3', component: Page3Component},
+  {path: 'vacio', component: VoidComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'vacio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { DefaultComponent } from './default/default.component';
+import { VoidComponent } from './void/void.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    DefaultComponent,
+    VoidComponent
   ],
   imports: [
     BrowserModule,
